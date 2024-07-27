688559eb77d8e6c0a07e2e963d0c4e9dc97d178d
AJ-1001 <asimjasim290901@gmail.com>
2024-07-27 20:16:51 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:disaster_management/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
-void main() {
+Future<void> main() async {
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
