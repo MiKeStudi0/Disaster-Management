10b5879b2001e4a654fa2997eb52151cdb485576
AJ-1001 <asimjasim290901@gmail.com>
2025-03-16 17:11:22 +0530

    updated

--- lib/disaster/screen/rescue/const.dart
+++ lib/disaster/screen/rescue/const.dart
@@ -1,4 +1,4 @@
 const appId = 742933673;
 const appSign =
     "e4981077c880ff42938eb20126a37c76a812df73aee53f924c1541a7fa622e8a";
-const map = 'AIzaSyDpq0RYQJ7P3f5ldQATuPyhkWXZm2d9sh0';
+const map = '';
